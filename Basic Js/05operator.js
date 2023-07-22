aa9ceecd9b6ac3a2bed0d0dbf7c50315bf2287a5
Swarnava <swarnavamudi22@gmail.com>
2023-07-22 13:18:59 +0530

    Types of DataType avialable in js

---
+++ Basic Js/05operator.js
@@ -0,0 +1,40 @@
+//JavaScript has 8 Datatypes
+
+// 01) string "Mohit"
+let newString = "Mothit";
+console.log(typeof newString); // OP - string
+//(typeof use to check the which type of data it is ?)
+
+// 02) number 2, 4, 5.6
+let newNumber = 22;
+console.log(typeof newNumber); // OP - Number
+// Number = decimal value and float and double, any kind of number is consider as number data type.
+
+// 03) booleans
+let newbool = false;
+console.log(typeof newbool); // OP - boolean
+// boolesns = true or false
+
+// 04) undefined
+let myUndefi;
+console.log(typeof myUndefi); // OP - undefined
+
+// 05) null
+let myNull = "delete this value in next line using null";
+console.log(myNull); // OP - delete this value in next line using null
+
+myNull = null;
+console.log(myNull); // OP - null
+console.log(typeof myNull); // OP - object
+
+// 06) BigInt
+// All JavaScript numbers are stored in a a 64-bit floating-point format.
+// JavaScript BigInt is a new datatype (ES2020) that can be used to store integer values that are too big to be represented by a normal JavaScript Number.
+
+// 07) Symbol
+
+// 08) Object Datatype
+// The object data type can contain:
+        // 001) An object
+        // 002) An array
+        // 003) A date
