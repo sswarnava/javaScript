a759704e5d7b24cc90d494ff92cf08a7fce45c5e
Swarnava <swarnavamudi22@gmail.com>
2023-07-25 22:44:47 +0530

    Added array methods

--- Basic Js/08arrays.js
+++ Basic Js/08arrays.js
@@ -1,8 +1,8 @@
 // ---------- Arrays ----------------
-let fruits = ["apple", "mongo", "grapes"];
+let fruits = ["apple", "mango", "grapes"];
 // we can access entire arrays at a time using array name. ex- fruits
 console.log(fruits);
-// OP - ['apple', 'mongo', 'grapes']
+// OP - ['apple', 'mango', 'grapes']
 
 let number = [1, 2, 3, 4];
 console.log(number);
@@ -13,11 +13,11 @@ console.log(miexdDatatype);
 //OP - [1, 2, 4 'Hello', 'car']
 
 // We access arrays using index number also. First array index is 0 and secound is index 1 and third index is 2 and so on in a same order.
-// let fruits = ["apple", "mongo", "grapes"];
+// let fruits = ["apple", "mango", "grapes"];
 // indexPosition    0        1         2
 
 console.log(fruits[0]); // OP - apple
-console.log(fruits[1]); // OP - mongo
+console.log(fruits[1]); // OP - mango
 console.log(fruits[2]); // OP - grapes
 
 // We can change the value of any index position in our array.
@@ -34,4 +34,3 @@ console.log(myNewArrays);
 console.log(typeof fruits); // OP - object (So array is object)
 // If you want to check specificaly it is array or not than use- Array.isArray(array_name)
 console.log(Array.isArray(fruits)); // OP - true (Means it is array)
-
---
+++ Basic Js/09arraysMethod.js
@@ -0,0 +1,42 @@
+// ---------- Array methods -----------
+let fruits = ["apple", "mango", "grapes"];
+console.log(fruits); // OP - ['apple', 'mango', 'grapes']
+
+// 01) Array length
+console.log(fruits.length); // OP - 3
+
+// 02) Array toString()
+console.log(fruits.toString()); // OP - apple,mango,grapes
+
+// ----- pop and push is faster than shift and unshift --------
+
+// 03) Array push()  || (at the end added)
+fruits.push("banana", "green_apple"); // You can add one or multiple value
+console.log(fruits); // OP - ['apple', 'mango', 'grapes', 'banana', 'green_apple']
+
+// 04) Array pop() || (pop from the end)
+console.log(fruits.pop()); // OP - green apple (Only one can be pop at a time)
+
+// 05) Array unshift()
+// Unshift add the element from first
+fruits.unshift("green_apple"); // we can add one or more than one.
+console.log(fruits);
+// OP - ['green_apple', 'apple', 'mango', 'grapes', 'banana']
+
+// 06) Array shift()
+// shift remove the element from the first
+fruits.shift();
+console.log(fruits);
+// OP - ['apple', 'mango', 'grapes', 'banana']
+
+
+// 07) Array concat()
+const classGirls = ["Rashi", "Rohini"];
+const classBoys = ["Rohit", "Shaam"];
+
+const classTotal = classGirls.concat(classBoys);
+console.log(classTotal);
+// Op - ['Rashi', 'Rohini', 'Rohit', 'Shaam']
+
+// Array splice()
+// Array slice()
