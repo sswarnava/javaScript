5d98545eb9f4f703b5df988f756ed3a89f0a7745
Swarnava <swarnavamudi22@gmail.com>
2023-08-05 23:59:47 +0530

    Updated call method

---
+++ JS Object/03call_Apply_Bind_Create.js
@@ -0,0 +1,44 @@
+// ---- The JavaScript call() Method
+const user = {
+    fullInfo: function(){
+        console.log(this.firstName, this.age)
+    }
+}
+const user1 = {
+    firstName: "Rohan",
+    age: 15,
+    
+}
+const user2 = {
+    firstName: "Mohit",
+    age: 18,
+}
+user.fullInfo.call(user1); // OP - Rohan 15
+user.fullInfo.call(user2); // OP - Mohit 18
+
+// In a normal object and method where we have to assign a modhod with a property 
+// Example property means (about) but in call method we can do same work but without assign a property. 
+// Using call method we can call directly whitout any property. So this is the advantage.                 
+
+// function personInfo() {  // -- It's a method
+//     let personInfo = `Person Full Name is ${this.firstName} ${this.lastName}`;
+//     return personInfo;
+// }
+// const person1 = {
+//     firstName: "Rohit",
+//     lastName: "Joshi",
+//     about: personInfo // -- It's a propaerty
+// }
+
+// ---- The call() Method with Arguments
+const person = {
+    fullInfo: function(hobby, city){
+        console.log(this.firstName, this.age, hobby, city)
+    }
+}
+const person1 = {
+    firstName: "Rohit",
+    age: 19,
+}
+
+person.fullInfo.call(person1, "guiter", "kolkata"); // OP - Rohit 19 guiter kolkata
