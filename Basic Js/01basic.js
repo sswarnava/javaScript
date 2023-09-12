13318620130a5baafcc40186793a3fcb69d799c6
Swarnava <swarnavamudi22@gmail.com>
2023-09-12 22:17:38 +0530

    Notes Added

--- Basic Js/01basic.js
+++ Basic Js/01basic.js
@@ -5,13 +5,23 @@ hello world!
 */
 
 // 02) console.log use for display the output in console.
-console.log("Hello world!");
+console.log("Hello world!")
 
 // 03 Display the output in a webpage Using document.write
-document.write("Hello world!");
+// document.write("Hello world!")
 
+// 04 Display the output in table form - console.table
+let NumberOne = 34
+let NumberTwo = 56
+console.table([NumberOne, NumberTwo]) 
+// OutPut │ (index) │ Values │
+//        ├─────────┼────────┤
+//        │    0    │   34   │
+//        │    1    │   56   |
+// --------- table method return a array and typeof table is object.
 
 // ----------- Notes ----------
 // 1) How to use comments in js.
 // 2) console.log - To print the output.
 // 3) document.write - To dispaly the output in a webpage.
+// 4) console.table - It's interesting method to dispaly the output in a tabler from. I
