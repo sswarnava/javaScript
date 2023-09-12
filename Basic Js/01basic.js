4ab12cca3b72ae9f3cdcd565dff9edeefe1a1f2b
Swarnava <swarnavamudi22@gmail.com>
2023-09-12 21:35:04 +0530

    In 01basic js file, Added a new section, called Notes

--- Basic Js/01basic.js
+++ Basic Js/01basic.js
@@ -1,13 +1,17 @@
-// 01) console.log use for display the output in console.
-console.log("Hello world!");
-
-// 02) Use for comments in js  - { // } or { /* -- */ }
+// 01) Use for comments in js  - { // } or { /* -- */ }
 /* 
 hello world!
 - multiline Comment 
 */
 
+// 02) console.log use for display the output in console.
+console.log("Hello world!");
+
 // 03 Display the output in a webpage Using document.write
 document.write("Hello world!");
 
 
+// ----------- Notes ----------
+// 1) How to use comments in js.
+// 2) console.log - To print the output.
+// 3) document.write - To dispaly the output in a webpage.
