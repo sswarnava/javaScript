15c43e2df2d96d67975a16d7b9691781cf976691
Swarnava <swarnavamudi22@gmail.com>
2023-09-13 21:13:58 +0530

    Notes added

--- Basic Js/04datatypes.js
+++ Basic Js/04datatypes.js
@@ -1,44 +1,60 @@
-//JavaScript has 8 Datatypes
+// * ---------- JavaScript has 2 Datatypes (Primitive & non-Primitive) ---------- * 
 
-// 01) string "Mohit"
+
+// * ------- primitive data type (Total 7) ------ *
+
+//  01) ----- string "Mohit"
 let newString = "Mothit";
 console.log(typeof newString); // OP - string
 //(typeof use to check the which type of data it is ?)
 
-// 02) number 2, 4, 5.6
+// 02) ------- number (2, 4, 5.6, 7.9090956789)
 let newNumber = 22;
 console.log(typeof newNumber); // OP - Number
 // Number = decimal value and float and double, any kind of number is consider as number data type.
 
-// 03) booleans
+// 03) ------ booleans (true and false)
 let newbool = false;
 console.log(typeof newbool); // OP - boolean
 // boolesns = true or false
 
-// 04) undefined
+// 04) ------ undefined (variable is decleared but value is not assign)
 let myUndefi;
 console.log(typeof myUndefi); // OP - undefined
 
-// 05) null
-let myNull = "delete this value in next line using null";
-console.log(myNull); // OP - delete this value in next line using null
-//Basically, null is a primitive type of a variable in JavaScript. Many people consider it a bug in JavaScript as it considers null as an object. Changing or fixing this bug will break the existing codebase, so it is not yet changed. The type of null is an object. That is why it is treated as an object.
+// 05) ------ null (no value)
+let myNull = null;
+console.log(myNull); // OP - null
+console.log(typeof myNull); // OP - object
+// null is a primitive datatype. However, when using the typeof operator, it returns "object". 
+// Many people consider, it's a bug in Js. Changing or fixing this bug will break the existing codebase, so it is not yet changed. 
 
-// Difference null vs Undifiend
+// ---- Difference null vs Undifiend
 console.log(undefined === undefined) // true (both type and value are the same)
 console.log(null === null) // true (both type and value are the same)
 console.log(null == undefined) // true (but the "same value") ("==" check only value)
 console.log(null === undefined) // false (not the same type) ("===" check value type both)
 
-myNull = null;
-console.log(myNull); // OP - null
-console.log(typeof myNull); // OP - object
-
-// 06) BigInt
+// 06) ----- BigInt
 // All JavaScript numbers are stored in a a 64-bit floating-point format.
 // JavaScript BigInt is a new datatype (ES2020) that can be used to store integer values that are too big to be represented by a normal JavaScript Number.
 
-// 07) Symbol
+// 07) ----- Symbol
+
+
+
+
+
+
+
+// * ------- non-primitive ------ *
+// 08) ------ Object Datatype
+// The object data type can contain:
+// -- 001) An object
+// -- 002) An array
+// -- 003) A date
+
+// * ------------ operator ---------- *
 // = assign symbol                                        |  > grater than
 // == comparison symbol (equal)                           |  < less than
 // === comparison symbol with data type compare           |  >= grater equal
@@ -48,8 +64,5 @@ console.log(typeof myNull); // OP - object
 // / division                                             |  && and symbol
 // % Modules
 
-// 08) Object Datatype
-// The object data type can contain:
-        // 001) An object
-        // 002) An array
-        // 003) A date
+// ----------- Notes ----------
+// *-- typeof null is not null, it is an object. however, undefined typeof is undefined and number typeof is number. (we can say null is an exception).
