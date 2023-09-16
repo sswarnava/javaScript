9bcda31a932ac38b8fac2f650041ed3897f59d4c
Swarnava <swarnavamudi22@gmail.com>
2023-09-17 01:51:30 +0530

    Modify some point and added notes

--- Basic Js/09arraysMethod.js
+++ Basic Js/09arraysMethod.js
@@ -1,51 +1,49 @@
-// ---------- Array methods -----------
+// * ---------- Array methods ----------- * 
 let fruits = ["apple", "mango", "grapes"];
 console.log(fruits); // OP - ['apple', 'mango', 'grapes']
 
-// 01) Array length
+// -- 01) Array length
 console.log(fruits.length); // OP - 3
 
-// 02) Array toString()
+// -- 02) Array toString()
 console.log(fruits.toString()); // OP - apple,mango,grapes
 
-// ----- pop and push is faster than shift and unshift --------
-
-// 03) Array push()  || (at the end added)
+// -- 03) Array push()  || (at the end added)
 fruits.push("banana", "green_apple"); // You can add one or multiple value
 console.log(fruits); // OP - ['apple', 'mango', 'grapes', 'banana', 'green_apple']
 
-// 04) Array pop() || (pop from the end)
+// -- 04) Array pop() || [pop from the end]
 console.log(fruits.pop()); // OP - green apple (Only one can be pop at a time)
 
-// 05) Array unshift()
-// Unshift add the element from first
-fruits.unshift("green_apple"); // we can add one or more than one.
-console.log(fruits);
-// OP - ['green_apple', 'apple', 'mango', 'grapes', 'banana']
-
-// 06) Array shift()
-// shift remove the element from the first
+// -- 05) Array shift() [ shift remove the element from the first ]
 fruits.shift();
-console.log(fruits);
-// OP - ['apple', 'mango', 'grapes', 'banana']
+console.log(fruits); // OP - ['apple', 'mango', 'grapes', 'banana']
 
-// 07) Array concat()
+// -- 06) Array unshift()  [Unshift add the element from first]
+fruits.unshift("green_apple"); // we can add one or more than one.
+console.log(fruits); // OP - ['green_apple', 'apple', 'mango', 'grapes', 'banana']
+
+// -- 07) Array concat()
 const classGirls = ["Rashi", "Rohini"];
 const classBoys = ["Rohit", "Shaam"];
-
 const classTotal = classGirls.concat(classBoys);
-console.log(classTotal);
-// Op - ['Rashi', 'Rohini', 'Rohit', 'Shaam']
+console.log(classTotal); // Op - ['Rashi', 'Rohini', 'Rohit', 'Shaam']
 
-// Array splice()
-// Array slice()
-let arraySlice = fruits.slice(0, 2); // - 0 and 1 index will be display
-console.log(arraySlice);
-// OP - ['apple', 'mango']
+// -- 08) Array slice()
+let myNum1 = [1, 2, 3, 4, 5, 6]
+let arraySlice = myNum1.slice(0, 3); 
+console.log(arraySlice); // OP - [ 1, 2, 3, 4, 5, 6 ]
+console.log(myNum1); // OP - [ 1, 2, 3 ]
 
-// ---------- Most Important array methods ---------
+// -- 09 Array splice() [splice method modify the original array]
+let arraySplice = myNum1.splice(0, 3);
+console.log(arraySplice); // OP - [ 1, 2, 3 ]
+console.log(myNum1); // OP - [ 4, 5, 6 ] 
 
-// ----- 01 forEach ----
+
+// * ---------- advance array methods --------- *
+
+// ----- 01) using forEach to acess the array item ----
 const numbers = [2, 6, 8, 7];
 
 function multipleBy2(number, index) {
@@ -61,24 +59,22 @@ numbers.forEach(multipleBy2);
 
 // forEach takes two input one is value and another is index position.
 
-// -------- 02 Map Methods ------
+// -------- 02) Using Map Methods acess the array item ------
 const square = (number) => {
   return number * number;
 };
 
 const squareNumber = numbers.map(square);
-console.log(squareNumber);
-// Output - [4, 36, 64, 49]
+console.log(squareNumber); // Output - [4, 36, 64, 49]
 
-// ---------- 03 Filter method
-
+// ---------- 03) Using Filter method ----
 const isEven = (number) => {
   return number % 2 === 0;
 };
 const evenNumbers = numbers.filter(isEven);
 console.log(evenNumbers);
 
-// ----------- 04reduce ------
+// ----------- 04) reduce ------
 const newNumbers = [1, 2, 3, 4, 5];
 const sum = newNumbers.reduce((accumulator, currentValue) => {
   return accumulator + currentValue;
@@ -90,3 +86,10 @@ console.log(sum);
 //     6              4           10
 //    10              5           15
 // Output is - 15
+
+// * ----- Notes ------ *
+// pop and push is faster than shift and unshift
+// splice methhod modify the original array
+// map use to acess the array data.
+// array.push() || array.pop() || array.length || array.slice || array.splice etc.
+// array.reduse also one importent. and filter also.
