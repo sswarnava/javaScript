4f94fe11fea574658451f6981f912a7cd4d764e5
Swarnava <swarnavamudi22@gmail.com>
2023-09-17 00:37:14 +0530

    Added few notes

--- Basic Js/08arrays.js
+++ Basic Js/08arrays.js
@@ -8,7 +8,7 @@ console.log(number); //OP - [1, 2, 3, 4]
 let miexdDatatype = [1, 2, 4, "Hello", "car"];
 console.log(miexdDatatype); //OP - [1, 2, 4 'Hello', 'car']
 
-// We access arrays using index number also. First array index is 0 and secound is index 1 and third index is 2 and so on in a same order.
+// ---- We access arrays using index number also. First array index is 0 and secound is index 1 and third index is 2 and so on in a same order.
 // let fruits = ["apple", "mango", "grapes"];
 // indexPosition    0        1         2
 
@@ -16,52 +16,8 @@ console.log(fruits[0]); // OP - apple
 console.log(fruits[1]); // OP - mango
 console.log(fruits[2]); // OP - grapes
 
-// ---- We can change the value of any index position in our array.
-fruits[1] = "banana";
-console.log(fruits); // OP - ['apple', 'banana', 'grapes']
-
-// ---- We can change the entire value of our arrays.
-fruits = ["a", "b", "c"];
-let myNewArrays = fruits;
-console.log(myNewArrays); // OP - [ 'a', 'b', 'c' ]
-console.log(fruits); // OP - [ 'a', 'b', 'c' ]
-// * Array follow shallow copy. means heep memory concept. If i change myNewArrays it will chage the value in both (newArray and as well as fruit.) array dont make copy, array point derictly reference.
-
-// typeof Array to check it's a array or not
-console.log(typeof fruits); // OP - object (So array is object)
-// If you want to check specificaly it is array or not than use- Array.isArray(array_name)
-console.log(Array.isArray(fruits)); // OP - true (Means it is array)
-
-// ------- Clone Array ----------
-let array1 = ["item1", "item2", "item3"];
-console.log(array1);
-// OP - ['item1', 'item2', 'item3']
-
-let array2 = array1.slice(0);
-console.log(array2);
-// OP - ['item1', 'item2', 'item3']
-
-// Here both are different array1 and array2
-// if i change array2 it will not reflalct in our array1 and same for array1 also
-
-array1.push("item4");
-console.log(array1); // OP - ['item1', 'item2', 'item3', 'item4']
-console.log(array2); // OP - ['item1', 'item2', 'item3']
-// We copy from array2 from array1 but now both are different.
-
-// Another way copy array using concat method:
-let array3 = [].concat(array2);
-array3.push("item4");
-console.log(array3);
-
-// Another way copy array using spread operator:
-let array4 = [...array3];
-array4.push("item5");
-console.log(array4);
-// OP - ['item1', 'item2', 'item3', 'item4', 'item5']
-
+// * ----- Get the value using for loop ----- *
 fruits = ["Banana", "Orange", "Apple", "Mango"];
-// -- Get the all value using for loop --
 for (let i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
@@ -71,7 +27,7 @@ for (let i = 0; i < fruits.length; i++) {
 // Apple
 // Mango
 
-// -- Get the all value using while loop --
+// -- Get the value using while loop --
 let i = 0;
 while (i < fruits.length) {
   console.log(fruits[i]);
@@ -83,7 +39,7 @@ while (i < fruits.length) {
 // Apple
 // Mango
 
-// --- Get the all value using do while loop ---
+// --- Get the value using do while loop ---
 i = 0;
 do {
   console.log(fruits[i]);
@@ -95,11 +51,53 @@ do {
 // Apple
 // Mango
 
-// array destructuring
+// ---- typeof Array
+console.log(typeof fruits); // OP - object (So array is object)
+// ---- If you want to check specificaly, is it array or not than use sy - Array.isArray(array_name)
+console.log(Array.isArray(fruits)); // OP - true (Means it is array)
 
+// ---- We can change the value of any index position in our array.
+fruits[1] = "banana";
+console.log(fruits); // OP - ['apple', 'banana', 'grapes']
+
+// ---- We can change the entire value of an arrays. (clone)
+fruits = ["a", "b", "c"];
+let myNewFruit = fruits;
+console.log(myNewFruit); // OP - [ 'a', 'b', 'c' ]
+console.log(fruits); // OP - [ 'a', 'b', 'c' ]
+// *----  Array follow shallow copy. means heap memory concept. If i change myNewArrays it will chage the value in both (newArray and as well as [myNewFruit].) array dont make copy, array point derictly reference.
+
+// * ------- Another method for Clone an Array ---------- *
+let array1 = ["item1", "item2", "item3"];
+console.log(array1); // OP - ['item1', 'item2', 'item3']
+
+let array2 = array1.slice(0);
+console.log(array2); // OP - ['item1', 'item2', 'item3']
+
+// Here both are different array1 and array2, means slice follow deep copy. (pass just a copy.)
+// if i change array2 it will not reflect in array1 and same for array1 also (beacuse here pass only array copy)
+
+array1.push("item4"); // push use to add a array item in last.
+console.log(array1); // OP - ['item1', 'item2', 'item3', 'item4']
+console.log(array2); // OP - ['item1', 'item2', 'item3']
+// Here I pushed a new item in array1 but array2 not change. But array2 copy from array1, and both are different value. (it's follow deep copy)
+
+// ----- Another way to copy array using concat method: -----
+let array3 = [].concat(array2);
+array3.push("item4");
+console.log(array3);
+
+// ----- Another way copy array using spread operator: ----- (recomended)
+let array4 = [...array3];
+array4.push("item5");
+console.log(array4);
+// OP - ['item1', 'item2', 'item3', 'item4', 'item5']
+
+
+// * -------- array destructuring ------------ *
 const myArray = ["value1", "value2"];
 
-// We can use this simple way But we don't use it -
+// ---- We can use this simple way But we don't use it beacause if 100 arrays are present we can't do like that-
 // let myVar1 = myArray[0];
 // let myVar2 = myArray[1];
 // console. log("value of myVar1", myVar1);
@@ -108,3 +106,14 @@ const myArray = ["value1", "value2"];
 // Mostly we use this type of array destructuring
 let [myVar1, myVar2] = myArray; // So here value1 will store myvar1 and same for value2 will store in myvar2
 console.log(myVar1); // OP - value1
+
+// * ----------- Notes ----------*
+// arrays can store same data type value or different data type value in a same array.
+// arrays can be acess using array name  
+// can be acess using index posintion.
+// can be acess using different loop.
+// typeof array is object
+// array follow shallow copy in genaral.
+// using slice method to clone an array, that time follow deep copy.
+// using spread clone an array is the best method.
+// destructuring is an one useful method to acess.
