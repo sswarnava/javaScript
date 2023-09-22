6e6fe307cb08ccb0ec3b5c3254fb2cc8f344de31
Swarnava <swarnavamudi22@gmail.com>
2023-09-22 23:51:39 +0530

    updated

--- Basic Js/07loop.js
+++ Basic Js/07loop.js
@@ -1,11 +1,10 @@
-// JavaScript supports different kinds of loops:
-
+// JavaScript support different kinds of loops
 // Every loop is has three part -
 // initial value. ( where should loop start And this value will update every time-loop).
 // condition ( will be there to check the condition. if condition will true then loop will run).
 // next is - increment OR decrement the value.
 
-// 01) while-loops through a block of code while a specified condition is true
+// * -- 01) while-loops through a block of code while a specified condition is true -- *
 let while_loops = 0;
 while (while_loops < 5) {
   while_loops++;
@@ -18,7 +17,7 @@ while (while_loops < 5) {
 // New Number is 4
 // New Number is 5
 
-// 02) do/while - also loops through a block of code while a specified condition is true
+// * -- 02) do/while - also loops through a block of code while a specified condition is true -- *
 let do_while = 50;
 // This loop check the condition at the end.
 // If Conditon is false loop we run one time atleast. Because condition check at the last.
@@ -34,7 +33,7 @@ do {
 // New Number is 54
 // New Number is 55
 
-// 03) for - loops through a block of code a number of times
+// * -- 03) for - loops through a block of code a number of times -- *
 for (let for_loops = 100; for_loops < 105; for_loops++) {
   // first variable use to store a value, this value use for starting point of any loop. From Where Should i start the loop help the first variable. Also we can say it's initial value and leter it will update every time after one loop completation. we can give any name for this variable name.
   // secound is one Condition from where to where loop will run, we set a conditin here.
@@ -49,7 +48,7 @@ for (let for_loops = 100; for_loops < 105; for_loops++) {
 // New Number is 103
 // New Number is 104
 
-// 04) for/in - loops through the properties of an object
+// * -- 04) for/in - loops through the properties of an object -- *
 
 // syntex -
 //      for (key in object) {
@@ -69,7 +68,7 @@ for (let x in person) {
 console.log(txt);
 //Output - John Doe 25
 
-// for/of - loops through the values of an iterable object
+// * -- 05) for/of - loops through the values of an iterable object -- *
 const cars = ["BMW", "Volvo", "Mini"];
 
 for (let x of cars) {
